a654f74eb4bfd1b6cb8774e3e9fc5adbb6d97097
Ryutaro Joe <ryuupigu623@gmail.com>
2023-03-21 16:32:11 +0900

    Initial Commit

---
+++ LibraryBookApp/LibraryBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryBookAppApp.swift
+//  LibraryBookApp
+//
+//  Created by 城龍太朗 on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LibraryBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
